7e1fdf9a2365934ea043091deaec31195f09cdf6
Volodymyr Mishchenko <volodymyr.mishchenko@strikersoft.com>
2025-10-26 12:05:18 +0200

    Update identity pool ID to new NonAuthentificateAccessRole

--- aws-exports.template.js
+++ aws-exports.template.js
@@ -3,7 +3,7 @@
 
 const awsmobile = {
     "aws_project_region": "us-east-1",
-    "aws_cognito_identity_pool_id": "us-east-1:5f91c797-0dd2-4531-a0de-43c660a39c1c",
+    "aws_cognito_identity_pool_id": "us-east-1:ee6b69d4-5dfb-4c08-bef5-87e0cad5a7a6",
     "aws_cognito_region": "us-east-1",
     "aws_user_pools_id": "us-east-1_hod4Ex5Eq",
     "aws_user_pools_web_client_id": "45ikktcbgv3fp4t1ptgcea98ma",
